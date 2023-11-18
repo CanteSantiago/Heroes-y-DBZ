17d4fd9b04e1b4dd1662527a458607c6c09026e6
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-18 00:03:09 -0500

    Vid52-TareaSobreModulos
    Crear el modulo sobre heores que contenga list y hero

--- Bases/src/app/app.module.ts
+++ Bases/src/app/app.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 import { counterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    ListComponent
+
   ],
   imports: [
     BrowserModule,
-    counterModule
+    counterModule,
+    HeroesModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
