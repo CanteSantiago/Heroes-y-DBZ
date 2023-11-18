0941d299cfc9e75b62e173d1a0f9d6d97e411876
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-18 01:05:15 -0500

    Vid61-PensemosEnPequeñosComponentes
    Crear component List

--- Bases/src/app/dbz/dbz.module.ts
+++ Bases/src/app/dbz/dbz.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+
 
 
 
 @NgModule({
-  exports:[
-    MainPageComponent
-  ],
   declarations: [
+    MainPageComponent,
+    ListComponent
+  ],
+  exports:[
     MainPageComponent
   ],
   imports: [
